b2e6eba39d1bfb9d7131569a173ed8c6b50821c6
jonac22 <jonathan.corallo.utn@gmail.com>
2016-12-08 13:17:29 -0300

    refactor code smell - template method

--- app/models/customer_search_term.rb
+++ app/models/customer_search_term.rb
@@ -13,10 +13,7 @@ class CustomerSearchTerm
   end
 
   def build_for_name_search(search_term)
-    @where_clause << case_insensitive_search(:first_name)
-    @where_args[:first_name] = starts_with(search_term)
-    @where_clause << " OR #{case_insensitive_search(:last_name)}"
-    @where_args[:last_name] = starts_with(search_term)
+    build_where_for_name(search_term)
     @order = "last_name asc"
   end
 
@@ -33,14 +30,19 @@ class CustomerSearchTerm
   end
 
   def build_for_email_search(search_term)
-    @where_clause << case_insensitive_search(:first_name)
-    @where_args[:first_name] = starts_with(extract_name(search_term))
-    @where_clause << " OR #{case_insensitive_search(:last_name)}"
-    @where_args[:last_name] = starts_with(extract_name(search_term))
+    build_where_for_name(search_term)
     @where_clause << " OR #{case_insensitive_search(:email)}"
     @where_args[:email] = search_term
     @order = "lower(email) = " +
         ActiveRecord::Base.connection.quote(search_term) +
         " desc, last_name asc"
   end
+
+  def build_where_for_name(search_term)
+    @where_clause << case_insensitive_search(:first_name)
+    @where_args[:first_name] = starts_with(extract_name(search_term))
+    @where_clause << " OR #{case_insensitive_search(:last_name)}"
+    @where_args[:last_name] = starts_with(extract_name(search_term))
+  end
+
 end
