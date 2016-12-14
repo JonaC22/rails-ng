deb654d3334561e472ef47be33c62e92e020c2bc
jonac22 <jonathan.corallo.utn@gmail.com>
2016-12-14 00:12:09 -0300

    Added angular-rails-templates

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,5 +14,7 @@
 //= require jquery_ujs
 //= require angular
 //= require angular-route
+//= require angular-rails-templates
+//= require_tree ./templates
 //= require turbolinks
 //= require_tree .
